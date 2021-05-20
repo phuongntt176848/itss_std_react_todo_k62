7d236147c1877e865a6e055dd81ed8765d4122c2
EC2 Default User <ec2-user@ip-172-31-62-214.ec2.internal>
2021-05-20 01:40:21 +0000

    Use Firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD98TXQr_bEMxs4gAYnN3OezAaqI0EtkQE",
+  authDomain: "fir-sample-444b2.firebaseapp.com",
+  projectId: "fir-sample-444b2",
+  storageBucket: "fir-sample-444b2.appspot.com",
+  messagingSenderId: "783870879516",
+  appId: "1:783870879516:web:ef91aa0b5f27e9a5497cd9"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="/__/firebase/8.6.1/firebase-app.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#available-libraries -->
+
+// <!-- Initialize Firebase -->
+// <script src="/__/firebase/init.js"></script>
+
+// npm install -g firebase-tools
