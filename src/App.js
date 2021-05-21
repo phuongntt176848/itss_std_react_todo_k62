7f1a4afb3f2e13005d1c4d3b4fe7031adcc6df27
EC2 Default User <ec2-user@ip-172-31-13-168.ec2.internal>
2021-05-21 09:51:37 +0000

    Save Avatar by Storage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
 /* スタイルシート */
 import './styles/main.css';
 
@@ -10,9 +9,59 @@ import Upload from "./components/Upload";
 import { auth, storeUserInfo , updateUser} from "./lib/firebase";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+   const [user, setUser] = useState([]);
+
+   useEffect(() => {
+     auth.onAuthStateChanged(async (user) => {
+       setLoading(false);
+       let newUser = null;
+       if (user) {
+         newUser = await storeUserInfo(user);
+       }
+       setUser(newUser);
+     });
+   }, []);
+   
+   const handleImageChanged = async downlodUrl => {
+    await updateUser(user, downlodUrl);
+  }
+
+   const logout = () => {
+     auth.signOut();
+   };
+
+   const HeaderContent = () => {
+     if (user) {
+       return (
+         <div class="navbar-end">
+           <div class="navbar-item">
+            <Upload userImage={user.image} onSletctedImage={handleImageChanged} />
+             {user.name}
+           </div>
+           <div class="navbar-item">
+             <button class="button is-danger is-light is-small" onClick={logout} > Logout</button>
+           </div>
+         </div >
+       )
+     } else {
+       return (<Login />)
+     }
+   }
   return (
     <div className="container is-fluid">
-      <Todo />
+    <header class="navbar">
+         {loading ? (
+           <p>
+             LOADING.....
+           </p>
+         ) : (
+           <HeaderContent />
+         )}
+       </header >
+       <div>
+         {user && <Todo />}
+       </div>
     </div>
   );
 }
